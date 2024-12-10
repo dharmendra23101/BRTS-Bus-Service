a0ab4fe0aa5e51a3650cd252a5b319ea26a73c55
Dhruw <dharmendra23101@iiitnr.edu.in>
2024-12-10 16:55:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function searchRoute() {
         alert('Please enter a starting point.');
     }
 }
+
+
+
+const messages = [
+    "Welcome to the Bus Tracker",
+    "Track Your Bus in Real-Time",
+    "Experience the Best BRT Service",
+    "Stay Informed About Routes & Fares",
+    "Plan Your Commute with Ease",
+  ];
+  
+  let messageIndex = 0;
+  const overlayText = document.getElementById("overlay-text");
+  
+  function updateText() {
+    overlayText.style.opacity = "0"; 
+    setTimeout(() => {
+      overlayText.textContent = messages[messageIndex]; 
+      overlayText.style.animation = "none"; 
+      overlayText.offsetHeight; 
+      overlayText.style.animation = "slideIn 1s ease forwards"; 
+      overlayText.style.opacity = "1"; 
+      messageIndex = (messageIndex + 1) % messages.length; 
+    }, 500); 
+  }
+  
+  setInterval(updateText, 3000); 
+  
